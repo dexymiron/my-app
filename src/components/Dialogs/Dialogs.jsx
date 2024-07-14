ad36d66883ef9a20f7d9b2297e39123262dd66c2
dexymiron <dexymiron@gmail.com>
2024-07-14 19:28:09 +0300

    third commit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,54 +2,49 @@ import React from 'react';
 import n from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
-let dialogsData = [
-    {id: 1, name:'Dimych' },
-    {id: 2, name:'Andrey' },
-    {id: 3, name:'Sveta' },
-    {id: 4, name:'Victor' },
-    {id: 5, name:'Valera' }
-]
-
-
-let messagesData = [
-    {id: 1, message:'Hi' },
-    {id: 2, message:'What are you doing?' },
-    {id: 3, message:'Hey' },
-    {id: 4, message:'Hey' },
-    {id: 5, message:'Hey' }
-]
-
 const DialogItem = (props) => {
-    let path= "/dialogs/" + props.id
-    return  (<div className={`${n.dialog} ${n.active}`}>
-                <NavLink to={path}>{props.name}</NavLink>
-             </div>
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={`${n.dialog} ${n.active}`}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
     );
-}
+};
 
 const Message = (props) => {
-    return <div className={n.message}>{props.message}</div>
-}
+    return <div className={n.dialog}>{props.message}</div>;
+};
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Victor'},
+        {id: 5, name: 'Valera'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'What are you doing?'},
+        {id: 3, message: 'Hey'},
+        {id: 4, message: 'Hey'},
+        {id: 5, message: 'Hey'}
+    ];
+
+    let dialogsElements = dialogsData.map(d => <DialogItem key={d.id} name={d.name} id={d.id} />);
+    let messagesElements = messagesData.map(m => <Message key={m.id} message={m.message} />);
+
     return (
         <div className={n.dialogs}>
             <div className={n.dialogItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                {dialogsElements}
             </div>
             <div className={n.messages}>
-                <Message message={messagesData[0].message} id={messagesData[0].id}/>
-                <Message message={messagesData[1].message} id={messagesData[1].id}/>
-                <Message message={messagesData[2].message} id={messagesData[2].id}/>
-                <Message message={messagesData[3].message} id={messagesData[3].id}/>
-                <Message message={messagesData[4].message} id={messagesData[4].id}/>
+                {messagesElements}
             </div>
         </div>
     );
-}
+};
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import n from './MyPosts.module.css';
 import Post from './Post/Post';
 
-let postsData = [
+const MyPosts = (props) => {
+  
+let posts = [
   {id: 1, message:'Hi how are you?' , likesCount: 15},
   {id: 2, message:'It\'s my first project', likesCount: 25},
   {id: 3, message:'It\'s my first project', likesCount: 35},
   {id: 4, message:'It\'s my first project', likesCount: 45},
 ]
 
-const MyPosts = () => {
+let postsElements = posts.map ( p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
     return (
     <div className={n.postsblock}>
       <h3>My posts</h3>
@@ -25,10 +27,7 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={n.posts}>
-        <Post id={postsData[0].id} message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-        <Post id={postsData[1].id} message={postsData[1].message} likesCount={postsData[1].likesCount}/>
-        <Post id={postsData[2].id} message={postsData[2].message} likesCount={postsData[2].likesCount}/>
-        <Post id={postsData[3].id} message={postsData[3].message} likesCount={postsData[3].likesCount}/>
+        { postsElements }
       </div>
     </div>);
 }
