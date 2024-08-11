cbefd644f6e2794ba0b64aa99837535b16b0a6c2
dexymiron <dexymiron@gmail.com>
2024-08-11 22:32:25 +0300

    14: connect axios/ create GET request for Users page / make Users component clear / add button 'Get Users'

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import n from './Friends.module.css';
 
 const Friends = (props) => {
-  debugger;
   const friendsElements = props.sidebar.map(friend => (
-   <span key={friend.id} className={n.friend}>
-           {friend.name}
-       </span>
- ))
-   return ( 
-     <div className={n.friends}>
-       {friendsElements}
-     </div>
-   )
+    <span key={friend.id} className={n.friend}>
+      {friend.name}
+    </span>
+  ))
+  return (
+    <div className={n.friends}>
+      {friendsElements}
+    </div>
+  )
 }
 
 export default Friends;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import n from './Users.module.css';
+import axios from "axios";
+import userPhoto from '../../assets/images/User.webp'
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-        props.setUsers(
-            [
+    let getUsers = () => {
 
-                {
-                    id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGlTvlfGD_6BhEo54FywLVrhwu0yDneo5k9IE9SVnByZSwToGikL87_Na-_CAVCoQBlSE&usqp=CAU',
-                    followed: false, fullname: 'Dmitriy', status: 'I like basketball', location: { city: 'Kiev', country: 'Ukraine' }
-                },
-                {
-                    id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGlTvlfGD_6BhEo54FywLVrhwu0yDneo5k9IE9SVnByZSwToGikL87_Na-_CAVCoQBlSE&usqp=CAU',
-                    followed: true, fullname: 'Max', status: 'I like play the guitar', location: { city: 'LosAngeles', country: 'USA' }
-                },
-                {
-                    id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGlTvlfGD_6BhEo54FywLVrhwu0yDneo5k9IE9SVnByZSwToGikL87_Na-_CAVCoQBlSE&usqp=CAU',
-                    followed: false, fullname: 'Misha', status: 'I like travel', location: { city: 'Oslo', country: 'Norway' }
-                }
-            ]
-        )
+
+        if (props.users.length === 0) {
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(responce => {
+
+                props.setUsers(responce.data.items);
+            });
+
+        }
     }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={n.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={n.userPhoto} />
                     </div>
                     <div>
                         {u.followed
@@ -44,8 +40,8 @@ let Users = (props) => {
 
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
