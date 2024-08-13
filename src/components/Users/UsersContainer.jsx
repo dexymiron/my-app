33a0941594e7c44b82cf3cbb912720e812924e3b
dexymiron <dexymiron@gmail.com>
2024-08-13 22:22:50 +0300

    17: refactoring: delegate server requests from Users to UsersContainer made Users component cleaner. delegated the task of server requests to the Container component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,6 +2,39 @@ import React from "react";
 import { connect } from "react-redux";
 import Users from "./Users";
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/users-reducer";
+import axios from "axios";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(responce => {
+            this.props.setUsers(responce.data.items);
+            this.props.setTotalUsersCount(responce.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(responce => {
+            this.props.setUsers(responce.data.items);
+        });
+    }
+
+    render() {
+
+
+        return <Users totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+    }
+}
+
 
 const mapStateToProps = (state) => {
     return {
@@ -32,4 +65,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
