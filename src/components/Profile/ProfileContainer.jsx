b5a5b649090d54422fb0f63222c08e198c4a5499
dexymiron <dexymiron@gmail.com>
2024-08-15 18:02:34 +0300

    21: add hooks useParams, useEffect when makes click on a User, displays his image on Profile page

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from "./Profile";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useParams } from "react-router-dom";
 
-class ProfileContainer extends React.Component {
-  componentDidMount() {
+const ProfileContainer = (props) => {
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const id = userId || 1;
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-      .then((responce) => {
-        this.props.setUserProfile(responce.data);
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + id)
+      .then((response) => {
+        props.setUserProfile(response.data);
       });
-  }
+  }, [userId, setUserProfile]);
 
-  render() {
-    return <Profile {...this.props} profile={this.props.profile} />;
-  }
-}
+  return <Profile {...props} profile={props.profile} />;
+};
 
-let mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
