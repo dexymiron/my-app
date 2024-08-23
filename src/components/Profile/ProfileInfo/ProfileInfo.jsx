29aea35b8b5b5e578a12bf4a1066bbc035e1a5d2
dexymiron <dexymiron@gmail.com>
2024-08-23 16:01:06 +0300

    29: added "status input" / state changing in local state / setState() -create ProfileStatus class component -doubleclicking on status field, causes changing status input -

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import n from "./ProfileInfo.module.css";
 import Preloader from "../../common/preloader/preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -10,11 +11,11 @@ const ProfileInfo = (props) => {
   return (
     <div>
       <div>
-        <img
+        {/*  <img
           className={n.image}
           src="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"
           alt="Background"
-        />
+        /> */}
       </div>
       <div className={n.descriptionBlock}>
         {props.profile.photos?.large ? (
@@ -27,6 +28,7 @@ const ProfileInfo = (props) => {
           />
         )}
         <p>{props.profile.fullName}</p>
+        <ProfileStatus status={"Put your status"} />
       </div>
     </div>
   );
