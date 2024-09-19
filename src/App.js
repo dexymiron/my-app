bb195d28c64d1ad6ec81d613caeb5ebdf8ddab5d
dexymiron <dexymiron@gmail.com>
2024-09-19 18:19:21 +0300

    44: added lazy loading to Profile/Dialogs pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import Music from './components/Music/Music';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+
 import SidebarContainer from './components/Friends/SidebarContainer';
 import NavbarContainer from './components/Navbar/NavbarConteiner';
 import UsersContainer from './components/Users/UsersContainer';
@@ -15,6 +15,12 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/preloader/preloader';
+import { lazy, Suspense } from 'react';
+
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
+//import ProfileContainer from './components/Profile/ProfileContainer';
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
 
 
 class App extends React.Component {
@@ -35,17 +41,19 @@ class App extends React.Component {
         <HeaderContainer />
         <NavbarContainer />
         <div className='app-wrapper-content'>
-          <Routes>
-            <Route path='/dialogs' element={<DialogsContainer />} />
-            <Route path='/profile/:userId?' element={<ProfileContainer />} />
-            <Route path="/profile" element={<ProfileContainer isMain={true}/>} /> 
-            <Route path='/news' element={<News />} />
-            <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
-            <Route path='/friends' element={<SidebarContainer />} />
-            <Route path='/users' element={<UsersContainer />} />
-            <Route path='/login' element={<LoginPage />} />
-          </Routes>
+        <Suspense fallback={<Preloader />}>
+            <Routes>
+              <Route path='/dialogs' element={<DialogsContainer />} />
+              <Route path='/profile/:userId?' element={<ProfileContainer />} />
+              <Route path="/profile" element={<ProfileContainer isMain={true}/>} /> 
+              <Route path='/news' element={<News />} />
+              <Route path='/music' element={<Music />} />
+              <Route path='/settings' element={<Settings />} />
+              <Route path='/friends' element={<SidebarContainer />} />
+              <Route path='/users' element={<UsersContainer />} />
+              <Route path='/login' element={<LoginPage />} />
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </BrowserRouter>
