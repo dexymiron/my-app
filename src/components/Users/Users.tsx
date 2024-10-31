91ef7028d124e471ed5a8fd700723f467756d342
dexymiron <dexymiron@gmail.com>
2024-10-31 17:49:47 +0200

    57: added changing URL params -when you use a filter or change a page, the url parameters changes -you can share the URL of cetrain page of users of filtered friends, with somebody

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,27 +1,19 @@
 import React, { useEffect } from "react";
 import n from "./Users.module.scss";
 import Paginator from "../common/Paginator/Paginator";
-// @ts-ignore
 import User from "./User";
 import UsersSearchForm from "./UsersSearchForm";
-import { FilterType, follow, requestUsers, unfollow} from "../../redux/users-reducer";
+import { FilterType, follow, requestUsers, unfollow } from "../../redux/users-reducer";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentPage, getFollowingInProgress, getPageSize, getTotalUsersCount, getUsers, getUsersFilter } from "../../redux/users-selectors";
 import { ThunkDispatch } from "redux-thunk";
 import { AppStateType } from "../../redux/redux-store";
 import { AnyAction } from "redux";
+import { useLocation, useNavigate } from "react-router-dom";
 
+type UsersType = {};
 
-
-
-type UsersType = {
-}
-
-
-
-
-export const Users: React.FC<UsersType> = (props) => {
-
+export const Users: React.FC<UsersType> = () => {
   const users = useSelector(getUsers);
   const totalUsersCount = useSelector(getTotalUsersCount);
   const currentPage = useSelector(getCurrentPage);
@@ -30,31 +22,57 @@ export const Users: React.FC<UsersType> = (props) => {
   const followingInProgress = useSelector(getFollowingInProgress);
 
   const dispatch: ThunkDispatch<AppStateType, unknown, AnyAction> = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  useEffect(()=>{
-    dispatch(requestUsers(currentPage, pageSize, filter));
-  }, [])
+  // Загрузка данных из URL-параметров при начальной загрузке компонента
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const page = searchParams.get("page");
+    const term = searchParams.get("term");
+    const friend = searchParams.get("friend");
 
+    const actualPage = page ? Number(page) : currentPage;
+    const actualFilter: FilterType = {
+      term: term || filter.term,
+      friend: friend === "true" ? true : friend === "false" ? false : null,
+    };
+
+    dispatch(requestUsers(actualPage, pageSize, actualFilter));
+  }, []); // Пустой массив зависимостей, чтобы загрузка происходила только один раз при монтировании
+
+  // Обновление URL и состояние при изменении фильтра
+  useEffect(() => {
+    const searchParams = new URLSearchParams();
+    if (filter.term) searchParams.set("term", filter.term);
+    if (filter.friend !== null) searchParams.set("friend", String(filter.friend));
+    if (currentPage !== 1) searchParams.set("page", String(currentPage));
+
+    navigate({
+      pathname: "/users",
+      search: searchParams.toString(),
+    });
+  }, [filter, currentPage, navigate]); // Обновление URL при изменении фильтра или страницы
 
   const onPageChanged = (pageNumber: number) => {
     dispatch(requestUsers(pageNumber, pageSize, filter));
-  }
+  };
 
   const onFilterChanged = (filter: FilterType) => {
-    dispatch(requestUsers(1, pageSize, filter));
-  }
+    dispatch(requestUsers(1, pageSize, filter)); // При изменении фильтра сбрасываем на первую страницу
+  };
 
   const handleFollow = (userId: number) => {
     dispatch(follow(userId));
   };
+
   const handleUnfollow = (userId: number) => {
     dispatch(unfollow(userId));
   };
 
-
   return (
     <div className={n.UsersPage}>
-      <UsersSearchForm onFilterChanged= {onFilterChanged}/>
+      <UsersSearchForm onFilterChanged={onFilterChanged} />
       <Paginator
         currentPage={currentPage}
         onPageChanged={onPageChanged}
@@ -73,3 +91,4 @@ export const Users: React.FC<UsersType> = (props) => {
     </div>
   );
 };
+
